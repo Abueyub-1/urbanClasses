825aecb3b7bf2b4708b9f8a217b99b5f41dd75d9
Ruslan <abueyub1@gmail.com>
2024-06-21 10:34:25 +0300

    6/21/2024

---
+++ lesson00.py
@@ -0,0 +1,12 @@
+print(5 // 5)  # // - целочисленное деление, / - деление
+print(type(2.0))
+print(25 * 5)
+print('Hello, World!')
+# concatenate
+print('Hello, ' + 'world!')  # вычитать строки нельзя
+# print('1' + 1) нельзя на питоне складывать разные типы
+print(True, False)  # boolean
+print(type(True), type(False))
+print(5 > 10)
+
+
