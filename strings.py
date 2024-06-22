ac0b541e6e2616af74c9020bf795de79c7def086
Ruslan <abueyub1@gmail.com>
2024-06-22 04:20:10 +0300

    lesson01 strings and indexation strings

---
+++ strings.py
@@ -0,0 +1,10 @@
+name = 'Aslan'
+print('Hello, ' + name)  # concatenation
+print('Hello, ' * 5)  # multiplying the string
+print(name[-1])  # indexation
+print(name[0:4])  # sequential indexing
+print(name[0:4])  # the colon is the beginning and the end
+print(name[0:3:2])  # adding a step
+print(name[:3])  # The beginning and the end can be skipped
+print(name[3:])  # On the contrary
+print(name[::-1])  # I skip the beginning and the end and specify the step - this is the line back
