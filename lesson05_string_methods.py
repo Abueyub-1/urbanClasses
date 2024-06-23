f6627b19bcde289dd43ae462b65e63e59fca217c
Ruslan <abueyub1@gmail.com>
2024-06-23 14:18:55 +0300

    sting methods less05

---
+++ lesson05_string_methods.py
@@ -0,0 +1,11 @@
+name = input('Введите Ваше имя: ')
+year_of_birth = int(input('Какого года рождения: '))
+current_year = 2024
+result = current_year - year_of_birth
+print('Здравствуйте! ', name)
+print('Тебе', result, 'лет')
+# перевод данных методами int str float
+print('привет, я строка в нижнем регистре'.upper().lower())
+print('привет, я строка в верхнем регистре'.replace('привет', 'пока').upper())
+print('привет, я строка в верхнем регистре'.replace(' ', '#'))
+# изучаем те методы которые чаще всего встречается
